50811723f567e0052dc887eac138a4029993ee7f
disunio <s19092@pjwstk.edu.pl>
2021-09-14 23:27:03 +0100

    s

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from gpiozero import LED, Pin, Button, DigitalInputDevice
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.g
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm test total test')
+    print_hi('PyCharm test total')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
