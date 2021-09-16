561a3a9efe868de82d8fee8a0312f946bcf5dc98
IziKodzik <ITI-guuy@gmail.com>
2021-09-17 01:07:03 +0200

    t

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     ledW = LED(20)
     ledY = LED(21)
     while True:
-        ledW.on()
-        ledY.off()
+        ledW.off()
+        ledY.on()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
