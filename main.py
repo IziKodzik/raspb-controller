9c77d1264ba81e105c54182f8b02e5e337663414
= <itiguuy@gmail.com>
2021-09-22 18:50:39 +0100

    tf

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from StepperMotor import StepperMotor
 if __name__ == '__main__':
     print('controller running')
     left_motor = PWMLED(18)
+    left_motor.off()
     while True:
-        left_motor.value = 0.3
-
-
+        left_motor.value = random.randrange(0, 100) / 100.0
