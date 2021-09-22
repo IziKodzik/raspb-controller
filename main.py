38305d78027f0fe309e1d4472c903f45e9fead54
= <itiguuy@gmail.com>
2021-09-22 23:35:40 +0100

    tf

--- main.py
+++ main.py
@@ -9,10 +9,5 @@ from StepperMotor import StepperMotor
 
 if __name__ == '__main__':
     print('controller running')
-    pwma = PWMLED(14)
-    pwma.value = 0.5
-    ao2 = LED(15)
-    ao2.on()
-    ao1 = LED(18)
-    ao1.off()
+
 
