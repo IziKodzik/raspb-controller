0cca0c5095c068a5cb593ff5a40219d9d3e92099
= <itiguuy@gmail.com>
2021-09-22 23:21:57 +0100

    xd

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from StepperMotor import StepperMotor
 if __name__ == '__main__':
     print('controller running')
     left_motor = Motor(23)
-    left_motor.go_with_speed(0.1)
+    left_motor.go_with_speed(1)
     sleep(4)
     left_motor.pwm_pin.off()
 
