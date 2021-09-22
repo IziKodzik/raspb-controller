47084dcbca7b8a9fc87c7eafbbbbb3e95a3398e9
= <itiguuy@gmail.com>
2021-09-22 23:56:11 +0100

    xd

--- main.py
+++ main.py
@@ -10,8 +10,5 @@ from StepperMotor import StepperMotor
 if __name__ == '__main__':
     print('controller running')
     led = PWMLED(2)
-    for j in range(0, 10):
-        for i in range(0, 100):
-            led.value = i / 100.0
-            sleep(0.01)
+
     led.off()
