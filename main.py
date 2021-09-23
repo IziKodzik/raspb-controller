198e5a68d8ed1e4c97a56cb597bb23c6179d2a8d
= <itiguuy@gmail.com>
2021-09-23 19:47:56 +0100

    TW

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from threading import Thread
 from time import sleep
 from datetime import datetime
 from Motor import Motor
+import RPi.GPIO
 from gpiozero import LED, PWMLED, DistanceSensor
 from StepperMotor import StepperMotor
 
 if __name__ == '__main__':
     print('controller running')
-    l = LED(2)
+    l = LED(17)
     while True:
         l.on()
