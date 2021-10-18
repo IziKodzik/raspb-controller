ca17aaadb13ef1bb1f38e9c9e5a15cd7f1f968f7
= <itiguuy@gmail.com>
2021-10-18 23:01:04 +0200

    tf

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ if __name__ == '__main__':
     print('controller running')
     right_motor = Motor(18, 15, 14)
     left_motor = Motor(22, 27, 17)
+
+    right_motor.go_with_speed(0.6)
+    left_motor.go_with_speed(1)
     while True:
-        right_motor.go_with_speed(0.6)
-        left_motor.go_with_speed(1)
+        print()
+        sleep(10)
+
