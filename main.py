baf54b8f1f5c9a6e4cfb6def55ac09b92099282a
= <itiguuy@gmail.com>
2021-10-18 23:00:22 +0200

    tf

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ if __name__ == '__main__':
     left_motor = Motor(22, 27, 17)
     while True:
         right_motor.go_with_speed(0.6)
-        left_motor.go_with_speed(0.3)
+        left_motor.go_with_speed(1)
