3926e6f96e9f7f92d888deed4b84478508e86986
= <itiguuy@gmail.com>
2021-10-18 23:13:44 +0200

    tf

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     left_motor = Motor(22, 27, 17)
     left_motor.toggle_direction()
 
-    right_motor.go_with_speed(0.6)
-    left_motor.go_with_speed(0.6)
+    right_motor.go_with_speed(1)
+    left_motor.go_with_speed(1)
     while True:
         right_motor.toggle_direction()
         left_motor.toggle_direction()
