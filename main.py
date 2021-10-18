2a13d77164f52252f65eb1da38471e770849e1ac
= <itiguuy@gmail.com>
2021-10-18 23:06:52 +0200

    tf

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     print('controller running')
     right_motor = Motor(18, 15, 14)
     left_motor = Motor(22, 27, 17)
+    left_motor.toggle_direction()
 
     right_motor.go_with_speed(0.6)
     left_motor.go_with_speed(0.6)
