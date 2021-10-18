086d1e2a5187a5cb2c0dab1608e18ca74c5e68df
= <itiguuy@gmail.com>
2021-10-18 22:42:59 +0200

    tf

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from StepperMotor import StepperMotor
 if __name__ == '__main__':
     print('controller running')
     pwm = PWMLED(18)
+    ain1 = LED(15)
+    ain1.on()
+    ain2 = LED(14)
     while True:
         for x in range(100):
             pwm.value = x/100.0
