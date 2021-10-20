90566a12e2f19299955555aaff371af8701f6c67
= <itiguuy@gmail.com>
2021-10-20 10:29:25 +0200

    ignore

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
     print('controller running')
     right_motor = Motor(18, 15, 14)
     left_motor = Motor(22, 27, 17)
+    left_motor.toggle_direction()
 
-    left_motor.go_with_speed(1)
-    right_motor.go_with_speed(1)
+    left_motor.go_with_speed(0.3)
+    right_motor.go_with_speed(0.3)
     while True:
         x = 1
