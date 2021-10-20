e6472d254f100f5722eae33e319fe1bb0ee516e5
= <itiguuy@gmail.com>
2021-10-20 11:40:45 +0200

    ignore

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import random
-import sys
-from threading import Thread
-from time import sleep
-from datetime import datetime
-import pygame
-
-import pygame.key
 
 from Motor import Motor
-from pygame.locals import *
-import RPi.GPIO
-from gpiozero import LED, PWMLED, DistanceSensor
-from StepperMotor import StepperMotor
 
 if __name__ == '__main__':
     print('controller running')
