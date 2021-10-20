06ca4205b655d386b9700ccb84afaaa17564fa28
= <itiguuy@gmail.com>
2021-10-20 12:05:58 +0200

    ignore

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
+import select
 
 from Motor import Motor
+import sys, pygame
+import termios
+import tty
+
+
+def isData():
+    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+
 
 if __name__ == '__main__':
     print('controller running')
+    pygame.init()
     right_motor = Motor(18, 15, 14)
     left_motor = Motor(22, 27, 17)
-    left_motor.toggle_direction()
-    left_motor.go_with_speed(1)
-    right_motor.go_with_speed(1)
-    while True:
-        xd = input("Enter")
-        if xd == 'w':
-            left_motor.toggle_direction()
+
+    old_settings = termios.tcgetattr(sys.stdin)
+    try:
+        tty.setcbreak(sys.stdin.fileno())
+
+        while True:
+            c = 0
+            if isData():
+                c = sys.stdin.read(1)
+                print(c)
+
+
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
