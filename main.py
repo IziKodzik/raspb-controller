b991199d5f3524ff496638045588f5e05253d4c6
= <itiguuy@gmail.com>
2021-10-22 00:04:15 +0200

    uwu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from time import sleep
-
+from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import DistanceSensor
 
 from Motor import Motor
@@ -10,7 +10,6 @@ from functools import partial
 
 def test(x):
     print(f'test {x}')
-    sys.exit()
 
 
 def prepare_robot():
@@ -20,7 +19,7 @@ def prepare_robot():
 
 
 if __name__ == '__main__':
-    collision_sensor = DistanceSensor(echo=23, trigger=24)
+    collision_sensor = DistanceSensor(echo=23, trigger=24, pin_factory=PiGPIOFactory())
     collision_sensor.threshold_distance = 0.05
     collision_sensor.when_deactivated = partial(test, 2)
     while True:
