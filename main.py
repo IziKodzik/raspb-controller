13d23a4edcdcb9eb5e53c8eb06efe95c0d0ecbcd
= <itiguuy@gmail.com>
2021-10-24 21:12:53 +0200

    s

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from functools import partial
 
 
 def prepare_robot():
-    right_motor = Motor(18, 15, 14)
+    right_motor = Motor(14, 15, 18)
     left_motor = Motor(22, 27, 17)
     return Robot(left_motor, right_motor)
 
 
 if __name__ == '__main__':
-    led = LED(14)
+    led = LED(15)
     led.on()
     sleep(100)
 
