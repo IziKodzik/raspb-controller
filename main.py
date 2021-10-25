b7f9953428c4b21ba86f0d07795d99c5e620d26e
= <itiguuy@gmail.com>
2021-10-25 22:15:45 +0200

    w

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ if __name__ == '__main__':
     print('?')
 
     sensor = DistanceSensor(echo=24, trigger=23, pin_factory=PiGPIOFactory())
-    while True:
-        print(sensor.distance)
+
     sensor.when_in_range = test
     sleep(100)
     print('??')
