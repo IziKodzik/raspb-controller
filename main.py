fc963c74a78d059379c7a9bbeb9c61e1dfbe703d
= <itiguuy@gmail.com>
2021-10-29 23:02:04 +0200

    w

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
-GPIO.setup(10, GPIO.IN,
+GPIO.setup(15, GPIO.IN,
            pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(18, GPIO.OUT,
            pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
