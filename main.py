4e43cb7ff35e727d26ff9af1b5c2c01dfa048b85
= <itiguuy@gmail.com>
2021-10-29 22:25:37 +0200

    w

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
-import datetime
-import os
-import sys
+import RPi.GPIO as GPIO
 import time
-from threading import Thread
-from time import sleep
-from gpiozero import DistanceSensor, LED
 
-from Motor import Motor
-from Robot import Robot
-from gpiozero.pins.pigpio import PiGPIOFactory
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
 
-from functools import partial
+# set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
 
-
-# 14 - yellow with black
-# 15 - green
-# 18 - black
-# 17 - blue
-# 27 - red
-# 22 - yellow
-
-def prepare_robot():
-    right_motor = Motor(14, 15, 18)
-    left_motor = Motor(22, 27, 17)
-    return Robot(left_motor, right_motor)
+# set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 
-def test(bo):
-    print('Noted:')
-    bo.turn_left()
+def distance():
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
+
+    return distance
 
 
 if __name__ == '__main__':
-    bot = prepare_robot()
-    sensor = DistanceSensor(echo=24, trigger=23, pin_factory=PiGPIOFactory())
-    sensor.threshold_distance = 0.7
-    # sensor.when_in_range = partial(test, bot)
-    end = time.time() + 40
-    while time.time() < end:
-        bot.go_forward()
-        while sensor.distance > 0.5 and time.time() < end:
-            1
-        bot.turn_left()
-        while sensor.distance < 0.5 and time.time() < end:
-            1
-    bot.stop()
+    try:
+        while True:
+            dist = distance()
+            print("Measured Distance = %.1f cm" % dist)
+            time.sleep(1)
+
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
