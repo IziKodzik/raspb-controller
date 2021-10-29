3f5d75af9b68910beaab39d559580566ace7584a
= <itiguuy@gmail.com>
2021-10-29 23:02:52 +0200

    w

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
 GPIO.setup(15, GPIO.IN,
            pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(18, GPIO.OUT,
-           pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(18, GPIO.OUT)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True:  # Run forever
     if GPIO.input(15) == GPIO.HIGH:
