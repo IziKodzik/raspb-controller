f2674b0b86c58172c1c1d9e350ad162a9b9cf647
= <itiguuy@gmail.com>
2021-10-29 23:12:41 +0200

    w

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ GPIO.setup(18, GPIO.OUT)  # Set pin 10 to be an input pin and set initial value 
 
 while True:  # Run forever
     GPIO.output(18, 1)
+    start = time.time()
     time.sleep(0.00001)
     GPIO.output(18, 0)
     if GPIO.input(15) == GPIO.HIGH:
-        print('negro')
+        print(start - time.time())
