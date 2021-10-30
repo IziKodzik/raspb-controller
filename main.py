1f4083fa84d3ff8156684a8cc51a752bf7292458
= <itiguuy@gmail.com>
2021-10-30 14:53:45 +0200

    'w'

--- main.py
+++ main.py
@@ -2,36 +2,40 @@ import time
 
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 
-GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
-GPIO.setup(24, GPIO.IN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(23, GPIO.OUT)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(14, GPIO.OUT)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+# GPIO.setwarnings(False)  # Ignore warning for now
+# GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
+# GPIO.setup(24, GPIO.IN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+# GPIO.setup(23, GPIO.OUT)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+# GPIO.setup(14, GPIO.OUT)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+#
+# GPIO.output(23, 0)
+# print('You ready?')
+# time.sleep(2)
+# print('Going...')
+from gpiozero import DistanceSensor
 
-GPIO.output(23, 0)
-print('You ready?')
-time.sleep(2)
-print('Going...')
-
-while True:
-    try:
-        GPIO.output(23, 1)
-        time.sleep(0.00001)
-        GPIO.output(23, 0)
-
-        while GPIO.input(24) == 0:
-            pulse_start = time.time()
-
-        while GPIO.input(24) == 1:
-            pulse_end = time.time()
-
-        pulse_duration = pulse_end - pulse_start
-
-        distance = pulse_duration * 17165
-        distance = round(distance, 1)
-        if distance < 30:
-            GPIO.output(14, 1)
-        else:
-            GPIO.output(14, 0)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+sensor = DistanceSensor(trigger=23, echo=24)
+sensor.wait_for_in_range()
+print('out')
+# while True:
+#     try:
+#         GPIO.output(23, 1)
+#         time.sleep(0.00001)
+#         GPIO.output(23, 0)
+#
+#         while GPIO.input(24) == 0:
+#             pulse_start = time.time()
+#
+#         while GPIO.input(24) == 1:
+#             pulse_end = time.time()
+#
+#         pulse_duration = pulse_end - pulse_start
+#
+#         distance = pulse_duration * 17165
+#         distance = round(distance, 1)
+#         if distance < 30:
+#             GPIO.output(14, 1)
+#         else:
+#             GPIO.output(14, 0)
+#     except KeyboardInterrupt:
+#         GPIO.cleanup()
