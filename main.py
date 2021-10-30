f51c3958871c04715a95768f12420917b127b958
= <itiguuy@gmail.com>
2021-10-30 19:46:46 +0200

    2

--- main.py
+++ main.py
@@ -12,34 +12,8 @@ try:
     led = LED(14)
     while True:
         if not sensor.in_range():
-            led.on()
+            print(time.time())
         else:
             led.off()
 except KeyboardInterrupt:
     GPIO.cleanup()
-
-#
-#
-# while True:
-#     try:
-#         GPIO.output(23, 1)
-#         time.sleep(0.00001)
-#         GPIO.output(23, 0)
-#
-#         while GPIO.input(24) == 0:
-#             pulse_start = time.time()
-#
-#         while GPIO.input(24) == 1:
-#             pulse_end = time.time()
-#
-#         pulse_duration = pulse_end - pulse_start
-#
-#         distance = pulse_duration * 17165
-#         distance = round(distance, 1)
-#         if distance < 30:
-#             GPIO.output(14, 1)
-#         else:
-#             GPIO.output(14, 0)
-#     except KeyboardInterrupt:
-#         GPIO.cleanup()
-#
