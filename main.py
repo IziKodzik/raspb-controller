1e1ba858eb40e3be409941bc4da38dff5f271c41
= <itiguuy@gmail.com>
2021-10-30 19:51:15 +0200

    'UltrasonicSensor-works'

--- main.py
+++ main.py
@@ -6,14 +6,4 @@ from gpiozero import LED
 from UltrasonicSensor import UltrasonicSensor
 
 GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
-try:
-    sensor = UltrasonicSensor(24, 23)
-    sensor.wait_for_in_range()
-    led = LED(14)
-    while True:
-        if not sensor.in_range():
-            print(time.time())
-        else:
-            led.off()
-except KeyboardInterrupt:
-    GPIO.cleanup()
+
