a90ef79bf229dfb059bd3602bde456901d42c641
= <itiguuy@gmail.com>
2021-10-30 20:39:47 +0200

    LaserSensor_to_add

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 # Main loop will read the range and print it every second.
 while True:
     print("Range: {0}mm".format(vl53.range))
-    time.sleep(1.0)
+    time.sleep(0.5)
