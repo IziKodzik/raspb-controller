a76bfab8423785dc6f071f74737e83874cf1fdf6
= <itiguuy@gmail.com>
2021-10-30 20:17:07 +0200

    'UltrasonicSensor-works'

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import time
 
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 from gpiozero import LED
+import board
+import busio
+import adafruit_vl53l0x
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_vl53l0x.VL53L0X(i2c)
 
-from UltrasonicSensor import UltrasonicSensor
 
-GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
-
+# GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
+while True:
+    print("Range: {0}mm".format(sensor.range))
+    time.sleep(1.0)
