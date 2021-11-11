4cd165d903fcc295d20fcdaad6116af74c220879
= <itiguuy@gmail.com>
2021-11-11 22:20:10 +0100

    w'

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from time import sleep
 import smbus2 as smbus
 sleep(0.1)
 bus = smbus.SMBus(1)
-sleep(1)
-for i in range(1024):
-    if bus.read_byte_data(0x1d,i) == 0:
-        print(i)
+
+while True:
+    bus.read_byte_data(0x1d, 4)
+    sleep(0.01)
 bus.close()
