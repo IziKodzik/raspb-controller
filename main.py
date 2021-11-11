8330d32ecfc0c172ab4a411111c4d2a6b5b032bb
= <itiguuy@gmail.com>
2021-11-11 22:52:27 +0100

    w'

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import busio
 import adafruit_lis3dh
 
 i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_lis3dh.LIS3DH_I2C(i2c,address=0x1d)
+accelerometer = adafruit_lis3dh.LIS3DH_I2C(i2c)
 
 while True:
     print("%f %f %f"%accelerometer.acceleration)
