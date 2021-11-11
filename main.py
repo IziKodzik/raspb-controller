b5e4e2c44cc2827ba23c26e64f8b09d30a955590
= <itiguuy@gmail.com>
2021-11-11 23:00:28 +0100

    w'

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
+
+import adafruit_vl53l0x
 import board
 import busio
 import adafruit_lis3dh
 
 i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_lis3dh.LIS3DH_I2C(i2c)
-
+thanks = adafruit_vl53l0x.VL53L0X(i2c)
 while True:
-    print("%f %f %f"%accelerometer.acceleration)
-    time.sleep(1)
+    print("%f"%thanks.distance)
+    time.sleep(0.1)
