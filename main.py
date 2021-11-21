3e7fa1e271640ede40846ad7ecf3a7f1be37d491
izikodzik <itiguuy@gmail.com>
2021-11-21 20:22:17 +0000

    uwu

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ from gpiozero import LED
 from StepperMotor import StepperMotor
 import busio
 from PIL import Image, ImageDraw, ImageFont
+WIDTH = 128
+HEIGHT = 32  # Change to 64 if needed
+BORDER = 5
 
 # motor_r = Motor(21, 20, 16)
 # motor_r.go_forward()
@@ -45,4 +48,24 @@ from PIL import Image, ImageDraw, ImageFont
 i2c = busio.I2C(SCL, SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128,64, i2c)
 oled.fill(0)
+oled.show()
+image = Image.new("1", (oled.width, oled.height))
+draw = ImageDraw.Draw(image)
+
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+draw.rectangle(
+    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+    outline=0,
+    fill=0,
+)
+font = ImageFont.load_default()
+text = "Hello World!"
+(font_width, font_height) = font.getsize(text)
+draw.text(
+    (oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2),
+    text,
+    font=font,
+    fill=255,
+)
+oled.image(image)
 oled.show()
