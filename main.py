114cd6ef18fd54d11d385cab0584f9e5a80207c3
izikodzik <itiguuy@gmail.com>
2021-11-21 20:07:22 +0000

    uwu

--- main.py
+++ main.py
@@ -55,11 +55,30 @@ padding = -2
 top = padding
 bottom = height - padding
 font = ImageFont.load_default()
-cmd = "hostname -I | cut -d' ' -f1"
-IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
-x = 0
-draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
-disp.image(image)
-disp.show()
 while True:
-    pass
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%d GB  %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write four lines of text.
+
+    draw.text((x, top+0), "IP: "+IP, font=font, fill=255)
+    draw.text((x, top+8), CPU, font=font, fill=255)
+    draw.text((x, top+16), MemUsage, font=font, fill=255)
+    draw.text((x, top+25), Disk, font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.show()
+    time.sleep(.1)
