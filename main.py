5ac95d856d8dfeb12b700cc0661bcd1a60a9457c
izikodzik <itiguuy@gmail.com>
2021-11-21 17:50:16 +0000

    uwu

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ import board
 # Initialize I2C bus and sensor.
 from gpiozero import LED
 
-from Motor import Motor
+# from Motor import Motor
 from StepperMotor import StepperMotor
+import busio
+# motor_r = Motor(21, 20, 16)
+# motor_r.go_forward()
+# motor_l = Motor(13, 19, 26)
+# motor_l.go_forward()
+#
+# stepper = StepperMotor(17, 27)
+# while True:
+#     time.sleep(0.01)
+#     stepper.take_step()
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
-motor_r = Motor(21, 20, 16)
-motor_r.go_forward()
-motor_l = Motor(13, 19, 26)
-motor_l.go_forward()
-
-stepper = StepperMotor(17, 27)
 while True:
-    time.sleep(0.01)
-    stepper.take_step()
+    print("Range: {0}mm".format(vl53.range))
+    time.sleep(1.0)
