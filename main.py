3d4b9be1fd4efebf31e6f09bfd49d69325ccaa30
izikodzik <itiguuy@gmail.com>
2021-11-21 19:39:10 +0000

    uwu

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 
 # Simple demo of the VL53L0X distance sensor.
 # Will print the sensed range/distance every second.
+import subprocess
 import time
 
 import adafruit_adxl34x
+import adafruit_ssd1306
 import adafruit_vl53l0x
 import board
 
+from board import SCL, SDA
 
 # Initialize I2C bus and sensor.
 from gpiozero import LED
@@ -16,6 +19,8 @@ from gpiozero import LED
 # from Motor import Motor
 from StepperMotor import StepperMotor
 import busio
+from PIL import Image, ImageDraw, ImageFont
+
 # motor_r = Motor(21, 20, 16)
 # motor_r.go_forward()
 # motor_l = Motor(13, 19, 26)
@@ -31,9 +36,49 @@ import busio
 # while True:
 #     print("Range: {0}mm".format(vl53.range))
 #     time.sleep(1.0)
-i2c = busio.I2C(board.SCL, board.SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
-
+# i2c = busio.I2C(board.SCL, board.SDA)
+# accelerometer = adafruit_adxl34x.ADXL345(i2c)
+#
+# while True:
+#     print("%f %f %f"%accelerometer.acceleration)
+#     time.sleep(1)
+i2c = busio.I2C(SCL, SDA)
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+disp.fill(0)
+disp.show()
+width = disp.width
+height = disp.height
+image = Image.new("1", (width, height))
+draw = ImageDraw.Draw(image)
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+padding = -2
+top = padding
+bottom = height - padding
+font = ImageFont.load_default()
+x = 0
 while True:
-    print("%f %f %f"%accelerometer.acceleration)
-    time.sleep(1)
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d' ' -f1"
+    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'cut -f 1 -d " " /proc/loadavg'
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write four lines of text.
+
+    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
+    draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
+    draw.text((x, top + 16), MemUsage, font=font, fill=255)
+    draw.text((x, top + 25), Disk, font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.show()
+    time.sleep(0.1)
