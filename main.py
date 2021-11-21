1fbda00e7d979cd474f468c167b9eaa8f7955c74
izikodzik <itiguuy@gmail.com>
2021-11-21 20:43:59 +0000

    uwu

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ BORDER = 5
 #     print("%f %f %f"%accelerometer.acceleration)
 #     time.sleep(1)
 i2c = busio.I2C(SCL, SDA)
-disp = adafruit_ssd1306.SSD1306_I2C(128,64, i2c)
+disp = adafruit_ssd1306.SSD1306_I2C(128,32, i2c)
 disp.begin()
 
 # Clear display.
