c1e296fa62d458f11048e18b97acead8b947f8f7
izikodzik <itiguuy@gmail.com>
2021-11-30 21:46:51 +0100

    iwi

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import time
 
 from Motor import Motor
 
-motor = Motor(16, 20, 21)
-motor.go_forward()
+motor1 = Motor(16, 20, 21)
+motor2 = Motor(26, 19, 13)
+
+motor1.go_forward()
+motor2.go_forward()
 time.sleep(3)
