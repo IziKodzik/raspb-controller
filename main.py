b82ad358e24ca35783a1db9e08afefb5e66fbbfe
izikodzik <itiguuy@gmail.com>
2021-12-01 01:14:46 +0100

    iwi

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import time
 
 from Motor import Motor
+from StepperMotor import StepperMotor
 
 motor1 = Motor(21, 20, 16)
 motor2 = Motor(13, 19, 26)
-
-motor1.go_forward()
-motor2.go_forward()
+stepper = StepperMotor(17, 27)
+while True:
+    stepper.take_step()
+# motor1.go_forward()
+# motor2.go_forward()
 time.sleep(3)
