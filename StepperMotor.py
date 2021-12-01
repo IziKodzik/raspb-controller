8e02bce677688d1386e4002556791a6736a015b3
izikodzik <itiguuy@gmail.com>
2021-12-01 08:13:05 +0100

    iwi

--- StepperMotor.py
+++ StepperMotor.py
@@ -11,9 +11,7 @@ class StepperMotor:
 
     def take_step(self):
         self.step_pin.off()
-        sleep(0.00000001)
         self.step_pin.on()
-        sleep(0.00000001)
 
     def change_dir(self):
         self.dir_pin.toggle()
