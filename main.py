466bba4f92216b538cd170acf7431da960c4255a
izikodzik <itiguuy@gmail.com>
2021-12-05 00:19:07 +0100

    iwi

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from StepperMotor import StepperMotor
 motor1 = Motor(21, 20, 16)
 motor2 = Motor(13, 19, 26)
 stepper = StepperMotor(17, 27)
+time.sleep(3)
+motor1.go_forward()
 motor2.go_forward()
 
 # while True:
