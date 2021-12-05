55c923bc24712132231b72809057d9508a32daf3
izikodzik <itiguuy@gmail.com>
2021-12-05 18:57:23 +0100

    iwi

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ motor1 = Motor(21, 20, 16)
 motor2 = Motor(13, 19, 26)
 stepper = StepperMotor(17, 27)
 time.sleep(1)
-motor1.go_forward()
-motor2.go_forward()
+# motor1.go_forward()
+# motor2.go_forward()
 
-# while True:
-#     for i in range(0, 200*8):
-#         stepper.take_step()
-#     time.sleep(1)
+while True:
+    for i in range(0, 200*8):
+        stepper.take_step()
+    time.sleep(1)
 
 # motor1.go_forward()
 # motor2.go_forward()
