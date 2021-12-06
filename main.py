1788ddb34742b4d0128e812c04cce7ae9d973636
izikodzik <itiguuy@gmail.com>
2021-12-06 19:08:21 +0100

    uwu

--- main.py
+++ main.py
@@ -10,17 +10,6 @@ from luma.core.interface.parallel import bitbang_6800
 from luma.core.render import canvas
 from luma.oled.device import sh1106
 
-# rev.1 users set port=0
-# substitute spi(device=0, port=0) below if using that interface
-# substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
-# serial = i2c(port=1, address=0x3C)
-
-# substitute ssd1331(...) or sh1106(...) below if using that device
-# device = sh1106(serial)
-# with canvas(device) as draw:
-#     draw.rectangle(device.bounding_box, outline="white", fill="black")
-#     draw.text((30, 40), "SUM", fill="white")
-
 motor1 = Motor(21, 20, 16)
 motor2 = Motor(13, 19, 26)
 stepper = StepperMotor(17, 27)
@@ -36,3 +25,11 @@ print(f"{vl53.range} mm")
 time.sleep(1)
 for i in range(4 * 200):
     stepper.take_step()
+
+serial = i2c(port=1, address=0x3C)
+
+device = sh1106(serial)
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), "SUM", fill="white")
+time.sleep(5)
