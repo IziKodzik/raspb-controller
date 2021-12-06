6ef9b524087bf8d07828b6cdb7ef737de43e929e
izikodzik <itiguuy@gmail.com>
2021-12-06 18:06:14 +0100

    iwi

--- main.py
+++ main.py
@@ -2,27 +2,16 @@ import time
 
 from Motor import Motor
 from StepperMotor import StepperMotor
+import board
+import busio
+import adafruit_vl53l0x
 
 motor1 = Motor(21, 20, 16)
 motor2 = Motor(13, 19, 26)
 stepper = StepperMotor(17, 27)
-time.sleep(2)
 
-for i in range(0, 200*8):
-    stepper.take_step()
-time.sleep(1)
-
-motor1.go_forward()
-motor2.go_forward()
-time.sleep(1)
-motor1.go_backward()
-motor2.go_backward()
-time.sleep(1)
-motor1.go_with_speed(0)
-motor2.go_with_speed(0)
-stepper.change_dir()
-for i in range(0, 200*8):
-    stepper.take_step()
-time.sleep(1)
-
-time.sleep(3)
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+while True:
+    print("Range: {0}mm".format(vl53.range))
+    time.sleep(1.0)
