2efcb285088b6dcbd1a9b06efd9a092f194c928b
izikodzik <itiguuy@gmail.com>
2021-12-08 12:26:34 +0100

    uwu

--- main.py
+++ main.py
@@ -12,19 +12,21 @@ stepper = StepperMotor(17, 27)
 
 i2c = busio.I2C(board.SCL, board.SDA)
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-x = []
-for i in range(200):
+for i in range(1600):
     stepper.take_step()
-    x.append(vl53.range)
-stepper.change_dir()
-print(x)
-time.sleep(1)
-
-for i in range(200):
-    stepper.take_step()
-
-motor1.go_forward()
-motor2.go_backward()
-time.sleep(1)
-motor1.stop()
-motor2.stop()
+# x = []
+# for i in range(200):
+#     stepper.take_step()
+#     x.append(vl53.range)
+# stepper.change_dir()
+# print(x)
+# time.sleep(1)
+#
+# for i in range(200):
+#     stepper.take_step()
+#
+# motor1.go_forward()
+# motor2.go_backward()
+# time.sleep(1)
+# motor1.stop()
+# motor2.stop()
