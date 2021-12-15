9b022dc5a2c36a9f6163ca55e8296c3f56b55a96
izikodzik <itiguuy@gmail.com>
2021-12-15 13:45:27 +0100

    iwi

--- StepperMotor.py
+++ StepperMotor.py
@@ -1,17 +1,20 @@
 from time import sleep
 
-from gpiozero import LED, PWMLED
+from gpiozero import LED
 
 
 class StepperMotor:
 
     def __init__(self, step_pin_no, dir_pin_no):
-        self.step_pin = PWMLED(step_pin_no)
+        self.step_pin = LED(step_pin_no)
         self.dir_pin = LED(dir_pin_no)
 
     def take_step(self):
-        self.step_pin.pulse(0.000000001, 0.000000001)
-
+        self.step_pin.off()
+        sleep(0.000000001)
+        self.step_pin.on()
+        sleep(0.000000001)
+        self.step_pin.off()
 
 
     def change_dir(self):
