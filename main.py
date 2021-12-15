699b1d0249ec5203505f8640682a093c7086d1d3
izikodzik <itiguuy@gmail.com>
2021-12-15 13:02:49 +0100

    iwi

--- main.py
+++ main.py
@@ -20,26 +20,44 @@ def count_hits(hi):
 hits = 0
 t = Thread(target=count_hits, args=(hits, ))
 t.start()
-motor1 = Motor(21, 20, 16)
-motor2 = Motor(13, 19, 26)
-motor1.go_forward()
-motor2.go_backward()
-time.sleep(1)
-sys.exit()
-stepper = StepperMotor(17, 27)
-
 i2c = busio.I2C(board.SCL, board.SDA)
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
-hits = 0
+motor1 = Motor(21, 20, 16)
+motor2 = Motor(13, 19, 26)
+stepper = StepperMotor(17, 27)
 
 x = []
+for i in range(1600):
+    distance = vl53.range
+    x.append(distance)
+    stepper.take_step()
+time.sleep(1)
+stepper.change_dir()
+for i in range(1600):
+    stepper.take_step()
+stepper.change_dir()
+print('============================================')
+for mes in x:
+    if (mes < 3000):
+        print(mes)
+    else:
+        print()
+x.clear()
+print('============================================')
+motor1.go_forward()
+motor2.go_forward()
+time.sleep(1)
+
 for i in range(1600):
     distance = vl53.range
     x.append(distance)
     print(distance)
     stepper.take_step()
 time.sleep(1)
+for i in range(1600):
+    stepper.take_step()
+stepper.change_dir()
 print('============================================')
 for mes in x:
     if (mes < 3000):
