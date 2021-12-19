e4e0bef47f8a7dfa1561549fe8e5a6e36efc5192
izikodzik <itiguuy@gmail.com>
2021-12-19 21:41:52 +0100

    w

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import sys
+import threading
 import time
 
 import requests
@@ -12,23 +13,35 @@ import busio
 import adafruit_vl53l0x
 from threading import Thread, Event
 
+
+def dupa():
+    current_thread = threading.currentThread
+    while not getattr(current_thread, "_stopped"):
+        print('gonwo')
+
+thread = Thread(target=dupa)
+thread._stopped = False
+thread.start()
+
 motor1 = Motor(21, 20, 16)
 motor2 = Motor(13, 19, 26)
 stepper = StepperMotor(17, 27)
 i2c = busio.I2C(board.SCL, board.SDA)
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-
 points = []
+
 
 for i in range(0, 1600):
     distance = vl53.range
     if distance > 8000:
         distance = 0
+
     if distance != 0:
         radians = i * 0.225 * math.pi / 180.0
         points.append({'x': (distance * math.sin(radians)), 'y': (distance * math.cos(radians))})
-    stepper.take_step()
 
+    stepper.take_step()
+thread._stopped = True
 mapPointsData = {'map-points': points}
 res = requests.post("http://192.168.0.115:8080/map-points", json=mapPointsData)
 stepper.change_dir()
