d000226f04dc8ea18f947c0a459770cd002e9fa0
izikodzik <itiguuy@gmail.com>
2021-12-23 00:08:15 +0100

    accel

--- Robot.py
+++ Robot.py
@@ -63,14 +63,15 @@ class Robot:
         spin = 0
 
         acceleration = self.accelerometer.acceleration
-        print(acceleration)
-        self.velocity = np.add(self.velocity, np.array(acceleration) * 0.001)
-        print(self.velocity)
-        sz = self.velocity[0] / 7.0
-        spin += sz / 0.001
-        self.shift[0] = self.shift[0] + self.velocity[1] * 0.001 * math.sin(spin)
-        self.shift[1] = self.shift[1] + self.velocity[1] * 0.001 * math.cos(spin)
-        time.sleep(0.001)
+        if abs(acceleration[0]) > 0.25 and abs(acceleration[1])> 0.25:
+            print(acceleration)
+            self.velocity = np.add(self.velocity, np.array(acceleration) * 0.001)
+            print(self.velocity)
+            sz = self.velocity[0] / 7.0
+            spin += sz / 0.001
+            self.shift[0] = self.shift[0] + self.velocity[1] * 0.001 * math.sin(spin)
+            self.shift[1] = self.shift[1] + self.velocity[1] * 0.001 * math.cos(spin)
+            time.sleep(0.001)
         while True:
             pass
 
