e1617752e9ccdc32b1031a85497ad9c5f16abcf5
izikodzik <itiguuy@gmail.com>
2022-01-09 20:31:22 +0100

    accel

--- Robot.py
+++ Robot.py
@@ -14,6 +14,10 @@ import busio
 import adafruit_vl53l0x
 import numpy as np
 from threading import Thread, Event
+from luma.core.interface.serial import i2c, spi, pcf8574
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
 
 
 class Robot:
@@ -47,6 +51,14 @@ class Robot:
 
     def display_image(self):
         print('xd')
+
+        # rev.1 users set port=0
+        # substitute spi(device=0, port=0) below if using that interface
+        # substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
+        serial = i2c(port=1, address=0x3C)
+
+        # substitute ssd1331(...) or sh1106(...) below if using that device
+        device = ssd1306(serial)
 
     def __init__(self):
         self.display_image()
