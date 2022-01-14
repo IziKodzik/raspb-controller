f7bd248c5a1ed8fbbe90fc041a0ea10396ed2ba6
izikodzik <itiguuy@gmail.com>
2022-01-14 13:48:13 +0100

    Double decoder

--- Decoder.py
+++ Decoder.py
@@ -6,7 +6,7 @@ class Decoder:
         self.out_pin = out_pin
         GPIO.setup(out_pin, GPIO.IN)
 
-    def wait_for_change(self, condition=True):
+    def wait_for_change(self, condition):
         first = GPIO.input(self.out_pin)
         while GPIO.input(self.out_pin) == first and condition:
             pass
